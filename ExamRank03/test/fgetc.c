b2f1aae4fed46476221ad2af7d69379bdf84c658
imad-touil <imadtouil.it@gmail.com>
2025-05-19 22:43:53 +0100

    ExamRank03

--- ExamRank03/test/fgetc.c
+++ ExamRank03/test/fgetc.c
@@ -3,17 +3,34 @@
 
 int	main(void)
 {
-	FILE	*f;
-	int		c;
+	// FILE	*f;
+	// int		c;
 
-	f = fopen("test.txt", "r");
-	c = fgetc(f);
-	c = ungetc(c, f);
-	printf("%c", c);
-	// while (c != EOF)
-	// {
-	// 	printf("%c", c);
-	// 	c = fgetc(f);
-	// 	// ungetc(c, f);
-	// }
+	// f = fopen("test.txt", "r");
+	// c = fgetc(f);
+	// // c = ungetc(c, f);
+	// printf("%c", c);
+	// // while (c != EOF)
+	// // {
+	// // 	printf("%c", c);
+	// // 	c = fgetc(f);
+	// // 	// ungetc(c, f);
+	// // }
+
+	FILE *fp = fopen("test.txt", "r");
+	if (fp != NULL)
+	{
+		int ch = fgetc(fp);
+		if (ch != EOF)
+		{
+			printf("Read character: %c\n", ch);
+			ungetc(ch, fp); // push it back
+			int ch2 = fgetc(fp);
+			printf("Re-read character: %c\n", ch2);
+		}
+	fclose(fp);
+	}
+
 }
+
+
